bfb640dfbc8e517cf3ba0aa4ea1f57ff97947501
PlanB <city7310@naver.com>
2017-10-20 12:38:05 +0900

    [UPDATE} API Description

--- Server/config.py
+++ Server/config.py
@@ -4,7 +4,10 @@ PORT = 7777
 
 API_VER = '0.1'
 API_TITLE = 'WakeHeart'
-API_DESC = '[ENDPOINT] http://52.79.134.200:' + str(PORT)
+API_DESC = '''
+[ENDPOINT] http://52.79.134.200:{0}
+서버에서 반환되는 Status code 중 401 UNAUTHORIZED는 JWT 토큰이 만료되었음을 뜻합니다.
+'''.format(PORT)
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 JWT_AUTH_URL_RULE = '/signin'
