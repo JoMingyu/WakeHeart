801c6794e5a12fe35455bccde283e3455ff2dbe5
PlanB <city7310@naver.com>
2017-11-02 11:31:11 +0900

    [FIX] Date Iteration Error

--- Server/routes/api/heart/heart_rate.py
+++ Server/routes/api/heart/heart_rate.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 
 from flask_restful_swagger_2 import Resource, request, swagger
 from flask_jwt import current_identity, jwt_required
@@ -16,9 +16,8 @@ class HeartRate(Resource):
     @jwt_required()
     def post(self):
         rate = request.form.get('rate', type=int)
-
-        HeartRateModel.objects(id=current_identity, date=str(date.today())).first().delete()
-        HeartRateModel(id=current_identity, rate=rate).save()
+        HeartRateModel.objects(_id=str(current_identity), date=str(date.today())).delete()
+        HeartRateModel(_id=str(current_identity), rate=rate).save()
 
         return '', 201
 
@@ -27,13 +26,13 @@ class HeartRate(Resource):
     def get(self):
         date_ = request.args.get('date')
 
-        heart_rate = HeartRateModel.objects(id=current_identity, date=date_).first()
+        heart_rate = HeartRateModel.objects(_id=str(current_identity), date=date_)
 
         if not heart_rate:
             return '', 204
         else:
             return {
-                'rate': heart_rate.rate
+                'rate': heart_rate.first().rate
             }, 200
 
 
@@ -41,16 +40,16 @@ class DateRangeBasedHeartRate(Resource):
     @swagger.doc(heart_rate_doc.DATE_RANGE_BASED_HEART_RATE)
     @jwt_required()
     def get(self):
-        start_date = request.args.get('start_date')
-        end_date = request.args.get('end_date')
+        start_date = datetime.strptime(request.args.get('start_date'), '%Y-%m-%d').date()
+        end_date = datetime.strptime(request.args.get('end_date'), '%Y-%m-%d').date()
 
         dates = [str(d) for d in daterange(start_date, end_date)]
         heart_rates = list()
         for date_ in dates:
-            heart_rate = HeartRateModel.objects(id=current_identity, date=date_).first()
+            heart_rate = HeartRateModel.objects(_id=str(current_identity), date=date_).first()
             heart_rates.append({
-                'date': heart_rate.date,
-                'rate': heart_rate.rate
+                'date': heart_rate.date if heart_rate else None,
+                'rate': heart_rate.rate if heart_rate else None
             })
 
         if not heart_rates:
